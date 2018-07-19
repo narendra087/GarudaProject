65ce5acda1877f95bda409d9b6f1a985c1e4cd4a
farihfadli <farihfadli44@gmail.com>
2018-07-19 14:35:40 +0700

    scene baru, font

--- GarudaProject/Assets/LoginToDatabase/DatabaseHandler.cs
+++ GarudaProject/Assets/LoginToDatabase/DatabaseHandler.cs
@@ -65,7 +65,7 @@ public class DatabaseHandler : MonoBehaviour {
     FirebaseApp app = FirebaseApp.DefaultInstance;
     // NOTE: You'll need to replace this url with your Firebase App's database
     // path in order for the database connection to work correctly in editor.
-    app.SetEditorDatabaseUrl("https://garudaproject-5105e.firebaseio.com/");
+    app.SetEditorDatabaseUrl("https://garudaproject-5105e.firebaseio.com//");
     if (app.Options.DatabaseUrl != null) app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
     StartListener();
   }
