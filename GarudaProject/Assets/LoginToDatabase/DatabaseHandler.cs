fc69fe6c3c3bc7e712f0f9005bd5dd052e182448
imamteguhislamy <notif.imam@gmail.com>
2018-07-24 01:01:46 +0700

    connect firebase

--- GarudaProject/Assets/LoginToDatabase/DatabaseHandler.cs
+++ GarudaProject/Assets/LoginToDatabase/DatabaseHandler.cs
@@ -51,7 +51,7 @@ public class DatabaseHandler : MonoBehaviour {
     FirebaseApp app = FirebaseApp.DefaultInstance;
     // NOTE: You'll need to replace this url with your Firebase App's database
     // path in order for the database connection to work correctly in editor.
-    app.SetEditorDatabaseUrl("https://garudaproject-5105e.firebaseio.com//");
+    app.SetEditorDatabaseUrl("https://garuda-word-3d21f.firebaseio.com//");
     if (app.Options.DatabaseUrl != null) app.SetEditorDatabaseUrl(app.Options.DatabaseUrl);
     StartListener();
   }
