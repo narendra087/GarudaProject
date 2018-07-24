c5e9501d8e28771963bce946d0003f90490db102
Narendra <akurendra26@gmail.com>
2018-07-24 16:48:30 +0700

    times up

--- GarudaProject/Assets/Script/Timer.cs
+++ GarudaProject/Assets/Script/Timer.cs
@@ -25,6 +25,22 @@ public class Timer : MonoBehaviour
         {
             StopCoroutine("LoseTime");
             GetComponent<TMPro.TextMeshProUGUI>().text = "Times Up!";
+            gm4.currentWord = "";
+            gm4.count = 0;
+            gm5.currentWord = "";
+            gm5.count = 0;
+            gm6.currentWord = "";
+            gm6.count = 0;
+            gm7.currentWord = "";
+            gm7.count = 0;
+            gm8.currentWord = "";
+            gm8.count = 0;
+            gm9.currentWord = "";
+            gm9.count = 0;
+            gm10.currentWord = "";
+            gm10.count = 0;
+            gm11.currentWord = "";
+            gm11.count = 0;
             SceneManager.LoadScene(newscene);
         }
     }
