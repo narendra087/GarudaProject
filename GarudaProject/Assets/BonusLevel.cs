f74130c6c1d40a3a053c6d64e3c84af3685136f2
Narendra <akurendra26@gmail.com>
2018-07-25 11:18:51 +0700

    asd

--- GarudaProject/Assets/BonusLevel.cs
+++ GarudaProject/Assets/BonusLevel.cs
@@ -2,35 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.Experimental.UIElements;
 
 public class BonusLevel : MonoBehaviour {
 
 	// Use this for initialization
-	public GameObject[] unlockBonus;
+	//public GameObject[] unlockBonus;
 	int nilaiB;
     public RectTransform Text;
+    public RectTransform hidup1;
+    public RectTransform hidup2;
+    public RectTransform hidup3;
 
     void Start () {
 		nilaiB = scoreS.nilai;
        // result.text = scoreS.nilai;
-         Text.GetComponent<TMPro.TextMeshProUGUI>().text = scoreS.nilai.ToString();
+        Text.GetComponent<TMPro.TextMeshProUGUI>().text = scoreS.nilai.ToString();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if (scoreS.nilai == 1111)
+		if (scoreS.nilai >= 1111)
 		{
-			unlockBonus[1].SetActive(false);
-		} else if (scoreS.nilai == 2222)
+			hidup1.gameObject.SetActive(false);
+			//unlockBonus[0].SetActive(false);
+		} else if (scoreS.nilai >= 2222)
 		{
-			unlockBonus[1].SetActive(false);
-			unlockBonus[2].SetActive(false);
-		} else if (scoreS.nilai == 3333)
+			hidup1.gameObject.SetActive(false);
+			hidup2.gameObject.SetActive(false);
+			// unlockBonus[0].SetActive(false);
+			// unlockBonus[1].SetActive(false);
+		} else if (scoreS.nilai >= 3333)
 		{
-			unlockBonus[1].SetActive(false);
-			unlockBonus[2].SetActive(false);
-			unlockBonus[3].SetActive(false);
+			hidup1.gameObject.SetActive(false);
+			hidup2.gameObject.SetActive(false);
+			hidup3.gameObject.SetActive(false);
+			// unlockBonus[0].SetActive(false);
+			// unlockBonus[1].SetActive(false);
+			// unlockBonus[2].SetActive(false);
 		}
 	}
 }
